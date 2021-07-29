1bfea57e0c915c3857bfdec1717bf30b9c299c6f
Rendy Arya Kemal <rendyarya22@gmail.com>
2021-07-29 07:48:58 +0700

    Implement fess deletion

--- pacilfess_discord/cogs/Fess.py
+++ pacilfess_discord/cogs/Fess.py
@@ -1,12 +1,17 @@
+import re
 from typing import Optional, TYPE_CHECKING
 from discord import Embed
 from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_option
-
+from datetime import datetime, timedelta
 from discord.ext.commands import Cog
 
 if TYPE_CHECKING:
     from pacilfess_discord.bot import Fess as FessBot
+
+DISCORD_RE = re.compile(
+    r"http[s]?:\/\/discord\.com\/channels\/(?P<GUILD>[0-9]+)\/(?P<CHANNEL>[0-9]+)\/(?P<MESSAGE>[0-9]+)"
+)
 
 
 def create_embed(confession: str, attachment: Optional[str] = None):
@@ -62,6 +67,7 @@ class Fess(Cog):
             ctx.send("You are banned from sending a confession.")
             return
 
+        current_time = datetime.now()
         embed = create_embed(confession, attachment)
         fess_message = await self.bot.target_channel.send(embed=embed)
 
@@ -74,8 +80,71 @@ class Fess(Cog):
                 confession,
                 ctx.author_id,
                 ctx.author.name,
-                fess_message.created_at.timestamp(),
+                current_time.timestamp(),
             ),
+        )
+        await self.bot.db.commit()
+        await ctx.send("Done!", hidden=True)
+
+    @cog_ext.cog_slash(
+        name="delete",
+        description="Deletes a confession.",
+        guild_ids=[863499218449858570],
+        options=[
+            create_option(
+                name="link",
+                description="Confession's message url to remove.",
+                option_type=3,
+                required=False,
+            )
+        ],
+    )
+    async def _delete_fess(self, ctx: SlashContext, link: Optional[str] = None):
+        # https://discord.com/channels/<GUILD_ID>/<CHANNEL_ID>/<MESSAGE_ID>
+        current_time = datetime.now()
+        five_mins_ago = current_time - timedelta(minutes=5)
+        if link:
+            re_result = DISCORD_RE.search(link)
+            if not re_result:
+                await ctx.send("Invalid confession link!", hidden=True)
+                return
+
+            cur = await self.bot.db.execute(
+                "SELECT * FROM confessions "
+                + "WHERE author=? AND sendtime>=? AND message_id=? "
+                + "ORDER BY sendtime DESC",
+                (
+                    ctx.author_id,
+                    five_mins_ago.timestamp(),
+                    int(re_result.group("MESSAGE")),
+                ),
+            )
+        else:
+            cur = await self.bot.db.execute(
+                "SELECT * FROM confessions "
+                + "WHERE author=? AND sendtime>=? "
+                + "ORDER BY sendtime DESC",
+                (ctx.author_id, five_mins_ago.timestamp()),
+            )
+
+        confess = await cur.fetchone()
+        if not confess:
+            await ctx.send(
+                "No confession earlier than 5 minutes ago found.",
+                hidden=True,
+            )
+            return
+
+        confess_id: int = confess[0]
+        confess_msg = await self.bot.target_channel.fetch_message(confess_id)
+        await confess_msg.edit(
+            embed=create_embed("*This confession has been deleted by the author.*")
+        )
+
+        cur = await self.bot.db.cursor()
+        await cur.execute(
+            "DELETE FROM confessions WHERE message_id=?",
+            (confess_id,),
         )
         await self.bot.db.commit()
         await ctx.send("Done!", hidden=True)
