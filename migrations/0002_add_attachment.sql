dbf5925d757805b6554632e39b0750546925d6f3
Rendy Arya Kemal <rendyarya22@gmail.com>
2021-08-13 11:08:41 +0700

    Remove old migrating tools

--- migrations/0002_add_attachment.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE confessions
-ADD COLUMN attachment TEXT;
--- migrations/0004_add_violations.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE violations (
-    user_hash TEXT NOT NULL,
-    severity INTEGER NOT NULL,
-    timestamp INTEGER NOT NULL
-);
